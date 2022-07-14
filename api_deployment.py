215c3561cb6876dcfe004ea07bb561df0ac44df0
AdamWatson91 <adamwatson91@gmail.com>
2022-07-14 20:51:38 +0100

    Adds combined model to api development, yet to be tested. Awaiting combined model training to finish

--- api_deployment.py
+++ api_deployment.py
@@ -74,6 +74,38 @@ class BertCNN(torch.nn.Module):
         X = self.layers(X)
         return X
     
+    def predict(self, text):
+        with torch.no_grad():
+            x = self.forward(text)
+            return x
+    
+    def predict_proba(self, text):
+        with torch.no_grad():
+            x = self.forward(text)
+            return torch.softmax(x, dim=1)
+
+    def predict_classes(self, text):
+        with torch.no_grad():
+            x = self.forward(text)
+            return self.decoder[int(torch.argmax(x, dim=1))]
+
+class CombinedModel(nn.Module):
+    def __init__(self, embedding_size=768, out_size=13, decoder: dict = None, device='cpu'):
+        super(CombinedModel, self).__init__()
+        self.decoder = decoder
+        resnet50 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_resnet50', pretrained=True)
+        out_features = resnet50.fc.out_features
+        self.image_classifier = nn.Sequential(resnet50, nn.Linear(out_features, 128)).to(device)
+        self.text_classifier = BertCNN(num_classes=128).to(device)
+        self.main = nn.Sequential(nn.Linear(256, out_size)).to(device)
+
+    def forward(self, image_features, text_features):
+        image_features = self.image_classifier(image_features)
+        text_features = self.text_classifier(text_features)
+        combined_features = torch.cat((image_features, text_features), 1)
+        combined_features = self.main(combined_features)
+        return combined_features
+
     def predict(self, image):
         with torch.no_grad():
             x = self.forward(image)
@@ -103,6 +135,12 @@ with open('bert_decoder.pkl', 'rb') as f:
 text_classifer = BertCNN(num_classes=len(text_decoder), decoder = text_decoder)
 text_classifer.load_state_dict(torch.load('bert_model.pt', map_location='cpu'))
 
+# Set up combined model
+with open('combined_decoder.pkl', 'rb') as f:
+    combined_decoder = pickle.load(f)
+combined_classifer = CombinedModel(num_classes=len(combined_decoder), decoder = combined_decoder)
+combined_classifer.load_state_dict(torch.load('combined_model.pt', map_location='cpu'))
+
 app = FastAPI()
 
 @app.post('/image_predict')
@@ -124,6 +162,20 @@ def text_predict(text: str = Form(...)):
     prediction = text_classifer.predict(processed_text)
     probs = text_classifer.predict_proba(processed_text)
     classes = text_classifer.predict_classes(processed_text)
+    print(prediction)
+    print(probs)
+    print(classes)
+    return JSONResponse(status_code=200, content={'prediction': prediction.tolist(), 'probs': probs.tolist(), 'class': classes})
+
+@app.post('/text_image_predict')
+def text_predict(image: UploadFile = File(...), text: str = Form(...)):
+    img = Image.open(image.file)
+    processed_img = image_processor(img)
+    text = text
+    processed_text = text_processor(text)
+    prediction = combined_classifer.predict(processed_img, processed_text)
+    probs = combined_classifer.predict_proba(processed_img, processed_text)
+    classes = combined_classifer.predict_classes(processed_img, processed_text)
     print(prediction)
     print(probs)
     print(classes)
