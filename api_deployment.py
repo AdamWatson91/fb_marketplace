a43b6b1c8a6a19a69ddc5e6c68fcae1a851a5ab1
AdamWatson91 <adamwatson91@gmail.com>
2022-07-14 20:12:30 +0100

    add JSOn response

--- api_deployment.py
+++ api_deployment.py
@@ -15,10 +15,11 @@ from fastapi import Request
 from fastapi import UploadFile
 from fastapi import File
 import uvicorn
+from fastapi.responses import JSONResponse
 
 class ImageClassifier(nn.Module):
     def __init__(self,
-                num_classes, device, decoder):
+                num_classes, decoder: dict = None, device='cpu'):
         super(ImageClassifier, self).__init__()
         self.resnet50 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_resnet50', pretrained=True)
         out_features = self.resnet50.fc.out_features
@@ -30,25 +31,25 @@ class ImageClassifier(nn.Module):
         x = self.main(inp)
         return x
     
-    def predict(self, out_features):
+    def predict(self, image):
         with torch.no_grad():
-            x = self.forward(out_features)
+            x = self.forward(image)
             return x
     
-    def predict_proba(self, out_features):
+    def predict_proba(self, image):
         with torch.no_grad():
-            x = self.forward(out_features)
+            x = self.forward(image)
             return torch.softmax(x, dim=1)
 
-    def predict_classes(self, out_features):
+    def predict_classes(self, image):
         with torch.no_grad():
-            x = self.forward(out_features)
+            x = self.forward(image)
             return self.decoder[int(torch.argmax(x, dim=1))]
 
 image_processor = ImageProcessor()
 with open('image_decoder.pkl', 'rb') as f:
     image_decoder = pickle.load(f)
-img_classifer = ImageClassifier(num_classes=len(image_decoder), decoder = image_decoder, device='cpu')
+img_classifer = ImageClassifier(num_classes=len(image_decoder), decoder = image_decoder)
 img_classifer.load_state_dict(torch.load('image_model.pt', map_location='cpu'))
 
 app = FastAPI()
@@ -58,18 +59,19 @@ def dummy(x):
     print(x)
     return 'Hello'
 
-@app.post('/example')
+@app.post('/example2')
 def dummy_post(image: UploadFile = File(...)):
     img = Image.open(image.file)
     processed_img = image_processor(img)
+    print(processed_img)
     prediction = img_classifer.predict(processed_img)
     probs = img_classifer.predict_proba(processed_img)
     classes = img_classifer.predict_classes(processed_img)
     print(prediction)
     print(probs)
     print(classes)
-    return 'Bye'
+    return JSONResponse(status_code=200, content={'prediction': prediction.tolist(), 'probs': probs.tolesit(), 'class': classes})
 
 if __name__ == '__main__':
-    uvicorn.run('api_example:app', host='0.0.0.0', port=8080)
+    uvicorn.run('api_deployment:app', host='0.0.0.0', port=8080)
 
