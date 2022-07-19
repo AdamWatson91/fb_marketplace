df7bd0e22c88bcb1e53692c9f2a31870dab01041
AdamWatson91 <adamwatson91@gmail.com>
2022-07-19 20:26:46 +0100

    Add documentation to classifier_image.py

--- classifer_image.py
+++ classifer_image.py
@@ -2,14 +2,23 @@ import torch.nn as nn
 import torch
 
 class ImageClassifier(nn.Module):
+    """
+    This classifier applies transfer learning using the resnet50 model
+    for a multi-classification of image data.
+
+    Args:
+        num_classes (int): The number of potential classess in the
+                            classifciation problem
+    """
     def __init__(self,
-                num_classes, device):
+                num_classes: int, 
+                device: str):
         super(ImageClassifier, self).__init__()
         self.resnet50 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_resnet50', pretrained=True)
         out_features = self.resnet50.fc.out_features
         self.linear = nn.Linear(out_features, num_classes).to(device)
         self.main = nn.Sequential(self.resnet50, self.linear).to(device)
-    
+
     def forward(self, inp):
         x = self.main(inp)
         return x
