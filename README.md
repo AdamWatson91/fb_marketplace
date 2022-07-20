9c7044b45948188a200d4547b592f2417a3c795b
AdamWatson91 <adamwatson91@gmail.com>
2022-07-20 20:22:52 +0100

    Add docker files but require further dev for deployment. Adds documentation to README

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# fb_marketplace
+# Facebook Marketplace Search Ranking
+Facebook marketplace uses a Search Ranking system to help users better find what they are looking for. This is based on complex unstrucutred data like images and text. The marketplace leverages computer vision and natural language processing (NLP) techniques to deliver the service. For more information [see here](https://engineering.fb.com/2018/10/02/ml-applications/under-the-hood-facebook-marketplace-powered-by-artificial-intelligence/).
+
+This project aims to reproduce some of these AI powered features. In particular, training a multimodal deep neural network model that predicts the category of a product based on its image and text description.
+
+Once the model is trained, it is deployed by creating an API with FastAPI. This API so accepts images and text and applies the trained model to produce a prediction. 
+
+This programme is then containerised using Docker, and uploaded to an EC2 instance.
+
+# Source data
+There were three sources of data which were then cleansed, transformed, and combined. This data was sourced from gumtree to get product images and their relevant product descriptions among other features. The below explains the data source:
+
+- Products.csv - Tabular dataset holding data relevant to the product such as the uid, description, price and location. This data includes the label (category) that will be the target variable for the mulit-class classification prediction.
+- Images.csv - Tabular dataset holding data relevant to the images, including the uid for the product the image relates to.
+    - NOTE: Multiple images exist for some products
+- image_folder - This holds the actual image file in jpg format.
+    - NOTE: Not included here due to size 
+
+# Data preparation
+In order to prepare the data for training purposes it was required to both cleanse Tabular data and merge. In addition, a holdout sample of the images was created for downstream testing of the API.
+
+The following programs were created to prodvide the nescessary fucntionality for cleansing the data:
+
+- clean_tabular.py - This program creates a class (TabularCleanse) which includes various methods for common functions for data cleansing
+- clean_images.py - This program creates a class (ImageCleanse) which includes various methods for common functions for image cleansing.
+
+The above were then used in the following programs to perform the data cleanse:
+
+- cleanse_tabular.py - used to transform the products.csv data into the pickle file 'products_cleansed.pkl'. The key functions applied are
+    - Removing null values and outliers from price
+    - Converting price from string to float
+    - Splitting columns to increase features
+    - Dropping colunms not required for further development
+- cleanse_images.py - used to transform the images and create a holdout sample of images. Creates pickle file 'cleansed_img_tabular' to hold data for those images not in the holdout sample. The following transformations are applied:
+    - Images are resized to 256x256 using the same ratio as input with black padding applied.
+- cleanse_product_images.py - used to merge and transform pickle files 'products_cleansed.pkl' & 'cleansed_img_tabular' to create one file 'cleansed_product_images.pkl'. Products without images are dropped.
+    - NOTE: 'cleansed_product_images.npy' also output in order to build and train model using google colab for gpu access.
+
+# Model training
+Three models were built, trained and validated using pytorch and transfer learning. The following programs were used:
+
+- model_train_bert.ipynb - trains text multi-class classification using transfer learning from bert embeddings. The following files are output:
+    - bert_decoder.pkl - dictionary with the class names and indexes applied during training.
+    - bert_model.pt - the trained model weights
+- model_train_resnet50.ipynb - trains image multi-class classfication using a CNN. Feature vector was created using transfer leavering (RESNET-50) with a classfication module added. The following files are output:
+    - image_decoder.pkl - dictionary with the class names and indexes applied during training.
+    - image_model.pt - the trained model weights
+- product_classification_mixed_modal.ipynb - This trains a mixed modal multi-class classification using both bert and RESNET-50. The following files are output:
+    - combined_decoder.pkl - dictionary with the class names and indexes applied during training.
+    - combined_model.pt - the trained model weights
+
+# Model deployment
+The trained models have been deployed using FastAPI and containerised using docker.
+
+The api is created in api_deployment.py. This provides the functionality to try all free models.
+
+In order to allow users to upload, text and images that can be trained using the models a processing step is required. processor_image.py and processor_text.py apply the required transformations to uploaded text or images for the model.
+
+# Other
+There are some other files included with details below. Please note these files were developed prior to final refactoring of the data preperation and may no longer work without further development
+
+- simple_img_classficiation.ipynb - This file was created to test image classficiation quality using machine learning rather than deep learning. Overall validation was around 18%
+- simple_regression.ipynb - This file uses traditional machine learning to perform regression for price prediction.
+- transfer_learning_vision_model.ipynb - This file was for intial development of a CNN for image classfication without transfer learning. Validation accuracy around 45%
---
+++ docker_folder/api_image.py
@@ -0,0 +1,208 @@
+import pickle
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from PIL import Image
+from fastapi import File
+from fastapi import UploadFile
+from fastapi import Form
+import torch
+import torch.nn as nn
+from processor_image import ImageProcessor
+from processor_text import TextProcessor
+import fastapi
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi import UploadFile
+from fastapi import File
+import uvicorn
+from fastapi.responses import JSONResponse
+
+class ImageClassifier(nn.Module):
+    def __init__(self,
+                num_classes, decoder: dict = None, device='cpu'):
+        super(ImageClassifier, self).__init__()
+        self.resnet50 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_resnet50', pretrained=True)
+        out_features = self.resnet50.fc.out_features
+        self.linear = nn.Linear(out_features, num_classes).to(device)
+        self.main = nn.Sequential(self.resnet50, self.linear).to(device)
+        self.decoder = decoder
+    
+    def forward(self, inp):
+        x = self.main(inp)
+        return x
+    
+    def predict(self, image):
+        with torch.no_grad():
+            x = self.forward(image)
+            return x
+    
+    def predict_proba(self, image):
+        with torch.no_grad():
+            x = self.forward(image)
+            return torch.softmax(x, dim=1)
+
+    def predict_classes(self, image):
+        with torch.no_grad():
+            x = self.forward(image)
+            return self.decoder[int(torch.argmax(x, dim=1))]
+
+class BertCNN(torch.nn.Module):
+    def __init__(self, embedding_size=768, num_classes=13, decoder: dict = None, device='cpu'):
+        super(BertCNN, self).__init__()
+        self.decoder = decoder
+        self.layers = torch.nn.Sequential(
+            nn.Conv1d(embedding_size, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.Conv1d(256, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.Conv1d(128, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.Conv1d(64, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(1152 , 128),
+            nn.ReLU(),
+            nn.Linear(128, num_classes)
+            ).to(device)
+
+    def forward(self, X):
+        X = self.layers(X)
+        return X
+    
+    def predict(self, text):
+        with torch.no_grad():
+            x = self.forward(text)
+            return x
+    
+    def predict_proba(self, text):
+        with torch.no_grad():
+            x = self.forward(text)
+            return torch.softmax(x, dim=1)
+
+    def predict_classes(self, text):
+        with torch.no_grad():
+            x = self.forward(text)
+            return self.decoder[int(torch.argmax(x, dim=1))]
+
+class CombinedBertCNN(torch.nn.Module):
+    def __init__(self, embedding_size=768, num_classes=13, device='cpu'):
+        super(CombinedBertCNN, self).__init__()
+        self.layers = torch.nn.Sequential(
+            nn.Conv1d(embedding_size, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.Conv1d(256, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.Conv1d(128, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.Conv1d(64, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(1152 , num_classes),
+            ).to(device)
+
+    def forward(self, X):
+        X = self.layers(X)
+        return X
+
+class CombinedModel(nn.Module):
+    def __init__(self, embedding_size=768, num_classes=13, decoder: dict = None, device='cpu'):
+        super(CombinedModel, self).__init__()
+        self.decoder = decoder
+        resnet50 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_resnet50', pretrained=True)
+        out_features = resnet50.fc.out_features
+        self.image_classifier = nn.Sequential(resnet50, nn.Linear(out_features, 128)).to(device)
+        self.text_classifier = CombinedBertCNN(embedding_size=embedding_size, num_classes=128).to(device)
+        self.main = nn.Sequential(nn.Linear(256, num_classes)).to(device)
+
+    def forward(self, image_features, text_features):
+        image_features = self.image_classifier(image_features)
+        text_features = self.text_classifier(text_features)
+        combined_features = torch.cat((image_features, text_features), 1)
+        combined_features = self.main(combined_features)
+        return combined_features
+
+    def predict(self, image_features, text_feature):
+        with torch.no_grad():
+            x = self.forward(image_features, text_feature)
+            return x
+    
+    def predict_proba(self, image_features, text_feature):
+        with torch.no_grad():
+            x = self.forward(image_features, text_feature)
+            return torch.softmax(x, dim=1)
+
+    def predict_classes(self, image_features, text_feature):
+        with torch.no_grad():
+            x = self.forward(image_features, text_feature)
+            return self.decoder[int(torch.argmax(x, dim=1))]
+
+# Set up image processing and model
+image_processor = ImageProcessor()
+with open('image_decoder.pkl', 'rb') as f:
+    image_decoder = pickle.load(f)
+img_classifer = ImageClassifier(num_classes=len(image_decoder), decoder = image_decoder)
+img_classifer.load_state_dict(torch.load('image_model.pt', map_location='cpu'))
+
+# Set up text processing and model
+text_processor = TextProcessor()
+with open('bert_decoder.pkl', 'rb') as f:
+    text_decoder = pickle.load(f)
+text_classifer = BertCNN(num_classes=len(text_decoder), decoder = text_decoder)
+text_classifer.load_state_dict(torch.load('bert_model.pt', map_location='cpu'))
+
+# Set up combined model
+with open('combined_decoder.pkl', 'rb') as f:
+    combined_decoder = pickle.load(f)
+combined_classifer = CombinedModel(num_classes=len(combined_decoder), decoder = combined_decoder)
+combined_classifer.load_state_dict(torch.load('combined_model.pt', map_location='cpu'))
+
+app = FastAPI()
+
+@app.post('/image_predict')
+def image_predict(image: UploadFile = File(...)):
+    img = Image.open(image.file)
+    processed_img = image_processor(img)
+    prediction = img_classifer.predict(processed_img)
+    probs = img_classifer.predict_proba(processed_img)
+    classes = img_classifer.predict_classes(processed_img)
+    print(prediction)
+    print(probs)
+    print(classes)
+    return JSONResponse(status_code=200, content={'prediction': prediction.tolist(), 'probs': probs.tolist(), 'class': classes})
+
+@app.post('/text_predict')
+def text_predict(text: str = Form(...)):
+    text = text
+    processed_text = text_processor(text)
+    prediction = text_classifer.predict(processed_text)
+    probs = text_classifer.predict_proba(processed_text)
+    classes = text_classifer.predict_classes(processed_text)
+    print(prediction)
+    print(probs)
+    print(classes)
+    return JSONResponse(status_code=200, content={'prediction': prediction.tolist(), 'probs': probs.tolist(), 'class': classes})
+
+@app.post('/text_image_predict')
+def text__image_predict(image: UploadFile = File(...), text: str = Form(...)):
+    img = Image.open(image.file)
+    processed_img = image_processor(img)
+    text = text
+    processed_text = text_processor(text)
+    prediction = combined_classifer.predict(processed_img, processed_text)
+    probs = combined_classifer.predict_proba(processed_img, processed_text)
+    classes = combined_classifer.predict_classes(processed_img, processed_text)
+    print(prediction)
+    print(probs)
+    print(classes)
+    return JSONResponse(status_code=200, content={'prediction': prediction.tolist(), 'probs': probs.tolist(), 'class': classes})
+
+if __name__ == '__main__':
+    uvicorn.run('api_deployment:app', host='0.0.0.0', port=8080)
+
